faa72b917527d50c573ae120c6ac939e75c24cfd
mwoodpatrick <mwoodpatrick@gmail.com>
2025-10-26 13:56:34 +0000

    move nil_ls to non mason lsp

--- init.lua
+++ init.lua
@@ -725,6 +725,26 @@ require('lazy').setup({
             -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
             settings = {},
           },
+
+          nil_ls = {
+            -- Command and arguments to start the server.
+            cmd = { vim.fn.expand '~/.nix-profile/bin/nil' },
+
+            -- Filetypes to automatically attach to.
+            filetypes = { 'nix' },
+
+            -- Sets the "root directory" to the parent directory of the file in the
+            -- current buffer that contains either a ".luarc.json" or a
+            -- ".luarc.jsonc" file. Files that share a root directory will reuse
+            -- the connection to the same LSP server.
+            -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+            root_markers = { 'flake.nix', '.git' },
+
+            -- Specific settings to send to the server. The schema for this is
+            -- defined by the server. For example the schema for lua-language-server
+            -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+            settings = {},
+          },
         },
         mason = {
           -- gopls = {},
@@ -771,25 +791,6 @@ require('lazy').setup({
               },
             },
           },
-          -- nil_ls = {
-          -- Command and arguments to start the server.
-          -- cmd = { vim.fn.expand '~/.nix-profile/bin/nil' },
-
-          -- Filetypes to automatically attach to.
-          -- filetypes = { 'nix' },
-
-          -- Sets the "root directory" to the parent directory of the file in the
-          -- current buffer that contains either a ".luarc.json" or a
-          -- ".luarc.jsonc" file. Files that share a root directory will reuse
-          -- the connection to the same LSP server.
-          -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-          -- root_markers = { 'flake.nix', '.git' },
-
-          -- Specific settings to send to the server. The schema for this is
-          -- defined by the server. For example the schema for lua-language-server
-          -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-          --   settings = {},
-          -- },
           bashls = {
             cmd = { 'bash-language-server', 'start' },
             settings = {
