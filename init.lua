0b4c45d9571bf4f46bfbe600ff24fbdac5096822
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2026-07-27 08:37:50 -0700

    Add key mappings for nvim-tree

--- init.lua
+++ init.lua
@@ -375,10 +375,28 @@ do
   vim.pack.add { gh 'NMAC427/guess-indent.nvim' }
   require('guess-indent').setup {}
 
+  -- Add a file explorer (Written In Lua)
   -- https://github.com/nvim-tree/nvim-tree.lua
   vim.pack.add { gh 'nvim-tree/nvim-web-devicons' }
   vim.pack.add { gh 'nvim-tree/nvim-tree.lua' }
+
+   local function nvim_tree_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.map.on_attach.default(bufnr)
+
+    -- custom mappings
+    vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent,        opts("Up"))
+    vim.keymap.set("n", "?",     api.tree.toggle_help,                  opts("Help"))
+  end
+
   require('nvim-tree').setup {
+    on_attach = nvim_tree_on_attach,
     sort = {
       sorter = 'case_sensitive',
     },
@@ -451,6 +469,7 @@ do
     end,
   }
 
+  -- Integrate Git functionality
   -- [fugitive.vim](https://github.com/tpope/vim-fugitive#fugitivevim)
   vim.pack.add { gh 'tpope/vim-fugitive' }
 
