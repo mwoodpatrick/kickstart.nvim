c9e3320c03027be50464d81146a4a2dd9d07aa4e
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2026-07-28 06:17:09 -0700

    Add yaml parser to nvim-treesitter

--- init.lua
+++ init.lua
@@ -1196,7 +1196,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
