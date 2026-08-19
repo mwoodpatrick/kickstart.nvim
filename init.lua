3079274a58205c75ed7955a3b47814d86b1bb1d5
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2026-08-19 05:53:39 -0700

    Seperate obsidian

--- init.lua
+++ init.lua
@@ -417,29 +417,6 @@ do
   -- [fugitive.vim](https://github.com/tpope/vim-fugitive#fugitivevim)
   vim.pack.add { gh 'tpope/vim-fugitive' }
 
-  vim.pack.add { gh 'obsidian-nvim/obsidian.nvim' }
-  require('obsidian').setup {
-    legacy_commands = false, -- Disable deprecated top-level global aliases
-    workspaces = {
-      {
-        name = 'personal',
-        path = '~/obsidian/vaults/personal', -- Adjust to your local vault path
-      },
-    },
-    -- Optional: mappings, note ID formatting, etc.
-    daily_notes = {
-      folder = 'notes/dailies',
-      date_format = '%Y-%m-%d',
-    },
-    ui = {
-      enable = false, -- Disables obsidian's UI engine to hand full control to render-markdown
-    },
-  }
-
-  -- Obsidian quick actions (Example bindings)
-  -- vim.keymap.set('n', '<leader>of', '<cmd>ObsidianQuickSwitch<CR>', opts)
-  -- vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianToday<CR>', opts)
-
   -- https://github.com/MeanderingProgrammer/render-markdown.nvim/wiki
   vim.pack.add { gh 'MeanderingProgrammer/render-markdown.nvim' }
   require('render-markdown').setup {
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,27 @@
+return function()
+  local utils = require 'user.utils'
+  local gh = utils.gh
+
+  vim.pack.add { gh 'obsidian-nvim/obsidian.nvim' }
+  require('obsidian').setup {
+    legacy_commands = false, -- Disable deprecated top-level global aliases
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/obsidian/vaults/personal', -- Adjust to your local vault path
+      },
+    },
+    -- Optional: mappings, note ID formatting, etc.
+    daily_notes = {
+      folder = 'notes/dailies',
+      date_format = '%Y-%m-%d',
+    },
+    ui = {
+      enable = false, -- Disables obsidian's UI engine to hand full control to render-markdown
+    },
+  }
+
+  -- Obsidian quick actions (Example bindings)
+  -- vim.keymap.set('n', '<leader>of', '<cmd>ObsidianQuickSwitch<CR>', opts)
+  -- vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianToday<CR>', opts)
+end
